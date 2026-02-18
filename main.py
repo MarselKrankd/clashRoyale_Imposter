dc38a83d4e7903cfb4a6ed23e64bf970c1e3191c
MarselKrankd <action.47@mail.ru>
2026-02-18 15:06:31 +0300

    создание комнаты

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from skelet import imposter
+from typing import List
+import random
+import uuid
+import string
+
+game_rooms={}
 
 app = FastAPI()
 
@@ -7,9 +13,17 @@ app = FastAPI()
 def read_root():
     return {"status": "Def noob API"}
 
-@app.get("/get-roles")
-def game_roles():
-    players=['rob','hog', 'ebenya']
+@app.post("/get-roles")
+def game_roles(players :List[str]):
     result=imposter(players)
     return result
+
+@app.post("/create-room")
+def create_room():
+    room_id=str(uuid.uuid4())
+    password="".join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    game_rooms[room_id] = {"password": password, "players": []}
+    return {"room_id": room_id, "password": password}
+    
+    
 
